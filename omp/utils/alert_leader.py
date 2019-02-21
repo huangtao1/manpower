c44e8d34ed590e4103e9decff68badecd489a758
huangtao <huangtao@oraro.net>
2019-02-21 08:48:20 +0800

    bug #1611 comments:每日邮件提醒 rootCause:每日邮件提醒 responseComments: solution:每日邮件提醒 testRerport:no testSuggestion:no
    Change-Id: Ia04d68a1b2e2030eb7cdb6e4a242b583cff81cca

--- omp/utils/alert_leader.py
+++ omp/utils/alert_leader.py
@@ -11,47 +11,52 @@ from email.mime.text import MIMEText
 from email.header import Header
 import time
 
-db_info = {'ip': '192.168.9.52', 'user': 'root', 'password': 'Aa123456', 'db_name': 'omp', 'port': 27}
-yesterday = (datetime.datetime.now() + datetime.timedelta(days=-4)).strftime("%Y-%m-%d")
+# db_info = {'ip': '192.168.9.52', 'user': 'root', 'password': 'Aa123456', 'db_name': 'omp', 'port': 27}
+yesterday = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime("%Y-%m-%d")
 print(yesterday)
 
-
-# db_info = {'ip': '192.168.8.240', 'user': 'root', 'password': '123456', 'db_name': 'omp', 'port': 3306}
+db_info = {'ip': '192.168.8.240', 'user': 'root', 'password': '123456', 'db_name': 'omp', 'port': 3306}
 
 
 def check_user_hour():
     mydb = Mydb(host=db_info['ip'], user_name=db_info['user'], password=db_info['password'], db_name=db_info['db_name'],
                 port=db_info['port'])
-    all_user_sql = "SELECT id,username,organize_id from omp_user"
-    get_hour_sql = "SELECT `hour` FROM omp_hours WHERE user_id={0} AND day='{1}'"
-    get_user_leader_sql = "SELECT username FROM omp_user WHERE organize_id={0} AND rank='leader'"
+    all_user_sql = "SELECT id,display_name,organize_id from omp_user"
+    get_hour_sql = "SELECT SUM(hour) FROM omp_hours WHERE user_id={0} AND day='{1}'"
 
     all_users = mydb.search_info(all_user_sql)
+    # 未达到8h的信息
+    no_enough_users = []
+    table_body = '<html><body><h4>Dear all:</h4><h4>%s日manpower统计中,工时未达到8小时的情况如下:</h4><table border="1" cellspacing="0" cellpadding="0"><thead><tr><th>姓名</th><th>工时</th></tr></thead>' % yesterday
     for user in all_users:
+        if user['display_name'] in ["田勇"]:
+            continue
         user_hour = mydb.search_info(get_hour_sql.format(user['id'], yesterday))
-        if user_hour:
-            # 填了,但是工时没有达到8h
-            pass
-        else:
+        if not user_hour[0]['SUM(hour)']:
+            # 没填工时
+            hour_info = {'name': user['display_name'], 'hour': 0}
+            no_enough_users.append(hour_info)
+        elif user_hour[0]['SUM(hour)'] < 8:
+            hour_info = {'name': user['display_name'], 'hour': user_hour[0]['SUM(hour)']}
+            no_enough_users.append(hour_info)
+    for no_enough_user in no_enough_users:
+        table_body = table_body + '<tr><td width="300" align="center">{0}</td><td width="300" align="center">{1}</td></tr>'.format(
+            no_enough_user['name'],
+            no_enough_user['hour'])
+    table_body = table_body + '</table><h4>请及时前往<a href="http://manpower.oraro.net">ManPower</a>填写!!!<h4>Best Regards<br/>Manpower@oraro.net</body></html>'
+    send_mail(user='huangtao', msg=table_body)
 
-            # 没有填写manpower
-            leader_name = mydb.search_info(get_user_leader_sql.format(user['organize_id']))[0]['username']
-            print(leader_name)
-            # 发送邮件提醒
-            send_mail(user=user['username'],cc_user=leader_name)
 
-
-def send_mail(user, cc_user):
+def send_mail(user, msg):
     # 设置服务器、用户名、口令及邮箱的后缀
     mail_host = 'smtp.263.net'
     mail_user = 'jenkins@oraro.net'
     mail_pass = 'Aa123456'
     sender = "jenkins@oraro.net"
     user = user + '@oraro.net'
-    cc_user = cc_user + '@oraro.net'
     msg_root = MIMEMultipart()
     # 解决中文乱码
-    all_msg = ''
+    all_msg = msg
     att_text = MIMEText(u'%s' % all_msg, 'html', 'UTF-8')
     # 添加邮件正文
     msg_root.attach(att_text)
@@ -63,7 +68,6 @@ def send_mail(user, cc_user):
     # 设置时间
     msg_root['Date'] = time.ctime(time.time())
     msg_root['To'] = ';'.join([user])
-    msg_root['Cc'] = ';'.join([cc_user])
 
     # 群发邮件
     smtp = smtplib.SMTP(mail_host, 25)
